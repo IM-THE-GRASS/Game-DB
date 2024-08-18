1418d5a4d4ea4ebc15a016e56a010713d2f97dff
IM-THE-GRASS <thegrassmanreplit@gmail.com>
2024-08-18 05:36:01 -0600

    make homepage

--- Game_DB/Game_DB.py
+++ Game_DB/Game_DB.py
@@ -13,27 +13,75 @@ class State(rx.State):
 
 def index() -> rx.Component:
     # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
+    return rx.box(
+        rx.image(
+            src="/gamedblogo.svg",
+            position="absolute",
+            
+            left="24vw",
+            top="17vh",
+            width="52vw",
+            height="21vh"
         ),
-        rx.logo(),
+        rx.box(
+            rx.hstack(
+                rx.input(
+                    font_size="60px",
+                    height="103px",
+                    width="100%"
+                ),
+                rx.center(
+                    
+                    rx.icon(
+                        "search",
+                        size=83
+                    ) ,
+                    width="103px",
+                    height="100%",
+                    padding="10px"
+                    
+                )
+            ),
+            
+            position="absolute",
+            width="626px",
+            height="103px",
+            left="647px",
+            top="408px",
+            border="1px solid #CEC8D4"
+        ),
+        
+        rx.box(
+            rx.hstack(
+                rx.image(
+                    width="146px",
+                    height="58px",
+                    src="/gamechip.svg"
+                ),
+                rx.image(
+                    width="146px",
+                    height="58px",
+                    src="/consolechip.svg"
+                ),
+                rx.image(
+                    width="146px",
+                    height="58px",
+                    src="/eventchip.svg"
+                ) ,
+                rx.image(
+                    width="146px",
+                    height="58px",
+                    src="/characterchip.svg"
+                ) 
+            ),
+            position="absolute",
+            top="527px",
+            left="648px",
+            width="625px",
+            height="58px"
+        ),
     )
 
 
-app = rx.App()
+app = rx.App(stylesheets=["./styles.css"])
 app.add_page(index)
